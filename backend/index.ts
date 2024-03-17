2d7448cddd90645b7b85d384d0ebd09b0a6a7a14
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-03-17 22:01:31 +0600

    Added  users router in index.ts and config.ts

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import expressWs from 'express-ws';
-import cors from 'cors'
+import cors from 'cors';
+import mongoose from 'mongoose';
+import config from './config';
+import usersRouter from "./routers/users";
 
 const app = express();
 expressWs(app);
 const port = 8000;
 
 app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
 const router = express.Router();
 
 router.ws('/chatRoom', (ws, req) => {
@@ -14,7 +19,17 @@ router.ws('/chatRoom', (ws, req) => {
 });
 
 app.use(router);
+app.use('/users', usersRouter);
+const run = async () => {
+    await mongoose.connect(config.mongoose.db);
 
-app.listen(port, () => {
-    console.log(`Server started on ${port} port!`);
-});
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port!`);
+    });
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+    });
+};
+
+void run();
