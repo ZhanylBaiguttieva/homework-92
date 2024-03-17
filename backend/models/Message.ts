36f99122b884054184d885b516e23031ea0a51ae
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-03-18 02:03:25 +0600

    Added Message.ts

---
+++ backend/models/Message.ts
@@ -0,0 +1,25 @@
+import {model, Schema, Types} from "mongoose";
+import User from "./User";
+
+const MessageSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        validate: {
+            validator: async (value: Types.ObjectId) => {
+                const user = await User.findById(value);
+                return Boolean(user);
+            },
+            message: 'User does not exist!',
+        },
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+});
+
+const Message = model('Message', MessageSchema);
+
+export default Message;
